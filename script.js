5f22bddb6e4635d697cee1d05082a20052661d07
Luke Criswell <luke@criswell.net>
2024-10-04 07:54:10 -0400

    Build `stewards_and_trustees.html` page and create CSS for portrait cards.

--- script.js
+++ script.js
@@ -13,16 +13,21 @@ const directions = document.querySelectorAll('.directions');
 const tabs = document.querySelectorAll('.tab');
 
 function showDirections(callingElement, id) {
-    
+    // Set the active tab
     tabs.forEach(tab => {
-        if(tab === callingElement)
+        if (tab === callingElement)
             tab.classList.add('active');
         else
             tab.classList.remove('active');
-    })
-    
+        
+        window.scrollY = 999;
+    });
+
+    // Hide all direction blocks
     directions.forEach(direction => {
         direction.style.display = 'none';
     });
+
+    // Show the desired direction block
     document.getElementById(id).style.display = 'block';
 }
